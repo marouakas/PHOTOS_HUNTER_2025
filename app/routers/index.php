5e0d4397017f57a088b41afa3f09b718719a57a0
marouakas <marouadevweb@gmail.com>
2025-07-26 19:03:05 +0200

    Route par défaut .pages.home

--- app/routers/index.php
+++ app/routers/index.php
@@ -1,4 +1,13 @@
 <?php
 
 // ROUTER PRINCIPAL 
+
+// ROUTE PAR DEFAUT
+// PATTERN: /
+
+// CTRL: pagesController
+// ACTION: homeAction
+
+include_once '../app/controllers/pagesController.php';
+\App\Controllers\PagesController\homeAction($connexion);
 ?>
