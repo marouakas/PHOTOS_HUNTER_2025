9cb388a7313880f6b31eb2cd88e577b9f21cd98c
marouakas <marouadevweb@gmail.com>
2025-07-27 00:54:44 +0200

    Helpers.truncate

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -22,7 +22,7 @@
                     </header>
 
                     <p class="text-base text-gray-600">
-                      <?php echo $photo['resume']; ?>
+                      <?php echo \Core\Helpers\truncate($photo['resume']); ?>...
                     </p>
                     <a
                       href="photo.html"
