77851f20c13347935c3f0108cf19a43b3a204439
marouakas <marouadevweb@gmail.com>
2025-07-30 21:02:50 +0200

    Route photos.index
    avec le partial _index

--- app/routers/index.php
+++ app/routers/index.php
@@ -2,12 +2,20 @@
 
 // ROUTER PRINCIPAL 
 
+// ROUTE DES PHOTOS 
+// PATTERN: /?photos
+// CTRL: photosController
+// ACTION: indexAction
+if (isset($_GET['photos'])):
+    include_once '../app/controllers/photosController.php';
+    \App\Controllers\PhotosController\indexAction($connexion);
 // ROUTE PAR DEFAUT
 // PATTERN: /
 
 // CTRL: pagesController
 // ACTION: homeAction
-
+else : 
 include_once '../app/controllers/pagesController.php';
 \App\Controllers\PagesController\homeAction($connexion);
+endif;
 ?>
--- app/views/templates/partials/_aside.php
+++ app/views/templates/partials/_aside.php
@@ -1,7 +1,7 @@
 <aside class="p-4 md:w-1/3">
 <div class="h-full p-6">
 
-  <?php  include '../app/views/partials/_search.php' ;  ?>
+  <?php  include '../app/views/templates/partials/_search.php' ;  ?>
   
   <?php  include '../app/views/categories/_index.php' ;  ?>
  </div>
